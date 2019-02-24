98239c21c27597a5af7bbd54d6f67115ce3cbce3
liam-armstrong <liam.armstrong124@gmail.com>
2019-02-24 10:41:56 -0800

    Test passing StripeColor functor

--- pa2/stripeColorPicker.cpp
+++ pa2/stripeColorPicker.cpp
@@ -2,10 +2,17 @@
 
 stripeColorPicker::stripeColorPicker(HSLAPixel fillColor, int stripeSpacing)
 {
-/* your code here! */
+    color = fillColor;
+    spacing = stripeSpacing;
 }
 
 HSLAPixel stripeColorPicker::operator()(int x, int y)
 {
-/* your code here! */
+    if((x % spacing) == 0){
+        return color;
+    } else {
+        HSLAPixel white;
+        white.l = 1;
+        return white;
+    }
 }
