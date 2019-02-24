748a6d56c580c558289250326a26476aad306d30
liam-armstrong <liam.armstrong124@gmail.com>
2019-02-24 14:09:38 -0800

    implemented borderColorPicker

--- pa2/borderColorPicker.cpp
+++ pa2/borderColorPicker.cpp
@@ -2,12 +2,28 @@
 
 borderColorPicker::borderColorPicker(HSLAPixel fillColor, PNG & img, double tolerance,HSLAPixel center)
 {
-/* Your code here! */
+    color = fillColor;
+    im = img;
+    tol = tolerance;
+    ctr = center;
 }
 
 HSLAPixel borderColorPicker::operator()(int x, int y)
 {
-
-/* Your code here! */
-    
+    for(int a = -3; a <= 3; a++){
+        for(int b = -3; b <= 3; b++){
+            if(((b*b) + (a*a)) > 9) {
+                continue;
+            }
+            if((x + a) >= (int)im.width() || (x + a) < 0 || (y + b) >= (int)im.height() || (y + b) < 0){
+                return color;
+            } else {
+                HSLAPixel * p = im.getPixel((x + a),(y + b));
+                if(p->h == color.h && p->l == color.l && p->s == color.s){
+                    return color;
+                }
+            }
+        }
+    }
+    return *im.getPixel(x, y);
 }
